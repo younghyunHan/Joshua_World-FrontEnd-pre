5827a83a78e10b175e60d9b26c72c8f235cd45c0
younghyunHan <88773996+younghyunHan@users.noreply.github.com>
2022-08-04 21:12:58 +0900

    Add: 게시글 작성 내용 state 관리

--- pages/Post/Post.tsx
+++ pages/Post/Post.tsx
@@ -1,15 +1,33 @@
+import { useState } from 'react';
 import ArticleStyles from './Post.module.css';
 
 export default function Post() {
+  const [postValues, setPostValues] = useState({
+    postTitle: '',
+    postContent: '',
+  });
+
+  const handlePostValue = (event: any) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    setPostValues({ ...postValues, [name]: value });
+  };
+
   return (
     <article id={ArticleStyles.post}>
-      <form id={ArticleStyles.postForm}>
+      <form id={ArticleStyles.postForm} onSubmit={handlePostValue}>
         <h1 id={ArticleStyles.postFormTitle}>게시글 작성</h1>
         <div id={ArticleStyles.postTitleWrap}>
           <label htmlFor='postTitle' id={ArticleStyles.postTitleLable}>
             제목
           </label>
-          <input type='text' id={ArticleStyles.postTitle} placeholder='Title' />
+          <input
+            type='text'
+            id={ArticleStyles.postTitle}
+            name='postTitle'
+            placeholder='Title'
+            onChange={handlePostValue}
+          />
         </div>
         <div id={ArticleStyles.postContentWrap}>
           <label htmlFor='postContent' id={ArticleStyles.postContentLable}>
@@ -17,7 +35,9 @@ export default function Post() {
           </label>
           <textarea
             id={ArticleStyles.postContent}
+            name='postContent'
             placeholder='게시글을 작성해주세요.'
+            onChange={handlePostValue}
           />
         </div>
         <button id={ArticleStyles.submitBtn}>작성완료</button>

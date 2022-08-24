7ef007661165158596eb652c4371c71b5bd95755
younghyunHan <88773996+younghyunHan@users.noreply.github.com>
2022-08-24 21:45:28 +0900

    Fix: 로그인 페이지 유효성 검사 구현

--- pages/SignIn/SignIn.tsx
+++ pages/SignIn/SignIn.tsx
@@ -1,22 +1,27 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import SignInStyles from './SignIn.module.css';
 import React from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export default function SignIn() {
-  const [loginValues, setLoginValues] = useState({
-    user_id: '',
-    user_pw: '',
-    user_name: '',
-  });
+  //이름, 이메일, 비밀번호, 비밀번호 확인
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [passwordConfirm, setPasswordConfirm] = useState<string>('');
+
+  //오류메시지 상태저장
+  const [emailMessage, setEmailMessage] = useState<string>('');
+  const [passwordMessage, setPasswordMessage] = useState<string>('');
+  const [passwordConfirmMessage, setPasswordConfirmMessage] =
+    useState<string>('');
+
+  // 유효성 검사
+  const [isEmail, setIsEmail] = useState<boolean>(false);
+  const [isPassword, setIsPassword] = useState<boolean>(false);
+  const [isPasswordConfirm, setIsPasswordConfirm] = useState<boolean>(false);
+
   const router = useRouter();
-
-  const handleInputValue = (e: any) => {
-    const { name, value } = e.target;
-    setLoginValues({ ...loginValues, [name]: value });
-  };
-
-  const { user_id, user_pw } = loginValues;
 
   const signIn = (event: any) => {
     event.preventDefault();
@@ -26,8 +31,8 @@ export default function SignIn() {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        user_id: user_id,
-        user_pw: user_pw,
+        user_id: email,
+        user_pw: password,
       }),
     })
       .then((response) => response.json())
@@ -43,27 +48,142 @@ export default function SignIn() {
       });
   };
 
+  // 이메일
+  const onChangeEmail = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const emailRegex =
+        /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+      const emailCurrent = e.target.value;
+      setEmail(emailCurrent);
+
+      if (!emailRegex.test(emailCurrent)) {
+        setEmailMessage('이메일 형식이 틀렸어요! 다시 확인해주세요 ㅜ ㅜ');
+        setIsEmail(false);
+      } else {
+        setEmailMessage('올바른 이메일 형식이에요 : )');
+        setIsEmail(true);
+      }
+    },
+    []
+  );
+
+  // 비밀번호
+  const onChangePassword = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const passwordRegex =
+        /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+      const passwordCurrent = e.target.value;
+      setPassword(passwordCurrent);
+
+      if (!passwordRegex.test(passwordCurrent)) {
+        setPasswordMessage(
+          '숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!'
+        );
+        setIsPassword(false);
+      } else {
+        setPasswordMessage('안전한 비밀번호에요 : )');
+        setIsPassword(true);
+      }
+    },
+    []
+  );
+
+  // 비밀번호 확인
+  const onChangePasswordConfirm = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const passwordConfirmCurrent = e.target.value;
+      setPasswordConfirm(passwordConfirmCurrent);
+
+      if (password === passwordConfirmCurrent) {
+        setPasswordConfirmMessage('비밀번호를 똑같이 입력했어요 : )');
+        setIsPasswordConfirm(true);
+      } else {
+        setPasswordConfirmMessage('비밀번호가 틀려요. 다시 확인해주세요 ㅜ ㅜ');
+        setIsPasswordConfirm(false);
+      }
+    },
+    [password]
+  );
+
   return (
     <div id={SignInStyles.signIn}>
       <form id={SignInStyles.signInForm}>
-        <h1 id={SignInStyles.signInFormName}>Blog</h1>
-        <input
-          id={SignInStyles.userId}
-          type='text'
-          name='user_id'
-          placeholder='전화번호, 사용자 이름 또는 이메일'
-          onChange={handleInputValue}
-        />
-        <input
-          id={SignInStyles.userPassword}
-          type='password'
-          name='user_pw'
-          placeholder='비밀번호'
-          onChange={handleInputValue}
-        />
-        <button type='button' id={SignInStyles.signInBtn} onClick={signIn}>
-          로그인
-        </button>
+        <div id={SignInStyles.signInFormTop}>
+          <h1 id={SignInStyles.signInFormName}>Blog</h1>
+          <div id={SignInStyles.signInFormTopContent}>
+            <div id={SignInStyles.signInTitle}>로그인</div>
+            <input
+              id={SignInStyles.userId}
+              type='text'
+              name='user_id'
+              placeholder='전화번호, 사용자 이름 또는 이메일'
+              onChange={onChangeEmail}
+            />
+            {email.length > 0 && (
+              <span
+                className={`${
+                  isEmail ? `${SignInStyles.success}` : `${SignInStyles.error}`
+                }`}
+              >
+                {emailMessage}
+              </span>
+            )}
+            <input
+              id={SignInStyles.userPassword}
+              type='password'
+              name='user_pw'
+              placeholder='비밀번호'
+              onChange={onChangePassword}
+            />
+            {password.length > 0 && (
+              <span
+                className={`${
+                  isPassword
+                    ? `${SignInStyles.success}`
+                    : `${SignInStyles.error}`
+                }`}
+              >
+                {passwordMessage}
+              </span>
+            )}
+            <input
+              id={SignInStyles.userPassword}
+              type='password'
+              name='user_pw_confirm'
+              placeholder='비밀번호 확인'
+              onChange={onChangePasswordConfirm}
+            />
+            {passwordConfirm.length > 0 && (
+              <span
+                className={`${
+                  isPasswordConfirm
+                    ? `${SignInStyles.success}`
+                    : `${SignInStyles.error}`
+                }`}
+              >
+                {passwordConfirmMessage}
+              </span>
+            )}
+          </div>
+        </div>
+        <div id={SignInStyles.SignInBtnWrap}>
+          <button
+            type='button'
+            className={`${SignInStyles.signInBtn} ${
+              isEmail && isPassword && isPasswordConfirm
+                ? `${SignInStyles.signInBtnLive}`
+                : ''
+            }`}
+            onClick={signIn}
+          >
+            로그인
+          </button>
+          <Link href='/SignUp/SignUp'>
+            <button type='button' id={SignInStyles.goToSignUp}>
+              회원가입
+            </button>
+          </Link>
+        </div>
       </form>
     </div>
   );

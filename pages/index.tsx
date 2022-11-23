30d9f08067b4968e08498664cb0821fe5a5a33fa
younghyunHan <atuniverse@naver.com>
2022-11-23 17:12:49 +0900

    Add: Main페이지 post부분 레이아웃 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -147,13 +147,73 @@ function Main() {
       </header>
       <main id={MainStyles.main}>
         <section id={MainStyles.informationTech}>
-          <h2 id={MainStyles.mainTitle}>HOME</h2>
+          <div id={MainStyles.mainTitleBox}>
+            <h2 id={MainStyles.mainTitle}>HOME</h2>
+            <div id={MainStyles.indicators}>
+              <span id={MainStyles.numberOfPosts}>+6 posts</span>
+              <span id={MainStyles.numberOfHashTags}>+13 hashtags</span>
+            </div>
+          </div>
           <div id={MainStyles.mainSubTitleBox}>
             <span id={MainStyles.mainSubTitleLeftLine} />
             <strong id={MainStyles.mainSubTitle}>Information Technology</strong>
             <span id={MainStyles.mainSubTitleRightLine} />
           </div>
+          <article id={MainStyles.mainPosts}>
+            {/* map */}
+            <img
+              id={MainStyles.mainPostsImg}
+              src="https://dersyb7nfifdf.cloudfront.net/blog/2021/08/frontend-backend-Copy.png"
+            />
+            <div id={MainStyles.mainContentBox}>
+              <h3 id={MainStyles.mainContentTitle}>
+                맨 땅에 헤딩하면서 배운 프론트엔드, 백엔드 성능 최적화
+              </h3>
+              <div id={MainStyles.mainContentHashTag}>
+                #성능 최적화 &nbsp;&nbsp;#리팩토링 &nbsp;&nbsp;#dry &nbsp;&nbsp;
+                #kiss
+              </div>
+              <div id={MainStyles.mainContent}>
+                구현이 전부가 아니다. 썸네일 출처 :
+                https://www.ironhack.com/en/web-development/front-end-vs-back-end-what-s-the-difference
+                프로젝트. 자바스크립트는 어떻게 돌아갈까? 브라우저 자바스크립트
+                엔진까지 알아보았습니다. 그렇다면 프론트엔드에서 자바스크립트는
+                어떻게...
+              </div>
+              <div>
+                <span id={MainStyles.mainContentTime}>9 days ago</span>
+                <span id={MainStyles.mainContentViews}>14 views</span>
+                <span id={MainStyles.mainContentLikes}>0 Likes</span>
+              </div>
+            </div>
+          </article>
         </section>
+        <aside>
+          <article>
+            {/* map */}
+            <h4>IN TECH 0 Likes</h4>
+            <h5>4. 자바스크립트 특징과 역사</h5>
+            <span>시간</span>
+            <span>views</span>
+            <span>0Likes</span>
+          </article>
+          <article>
+            {/* map */}
+            <h4>IN TECH 0 Likes</h4>
+            <h5>4. 자바스크립트 특징과 역사</h5>
+            <span>시간</span>
+            <span>views</span>
+            <span>0Likes</span>
+          </article>
+          <article>
+            {/* map */}
+            <h4>IN TECH 0 Likes</h4>
+            <h5>4. 자바스크립트 특징과 역사</h5>
+            <span>시간</span>
+            <span>views</span>
+            <span>0Likes</span>
+          </article>
+        </aside>
       </main>
     </>
     // <>

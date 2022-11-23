8706b321825091208ca288c239ffffef5bec78bf
younghyunHan <atuniverse@naver.com>
2022-11-23 15:20:56 +0900

    Fix: color-syntax plugin type Error해결 중

--- pages/Writer/Writer.tsx
+++ pages/Writer/Writer.tsx
@@ -1,20 +1,25 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import dynamic from 'next/dynamic';
-// import { useEffect, useState } from 'react';
-// import axios from 'axios';
+
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+// import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 
 import Nav from '../Components/Nav';
 
-import '@toast-ui/editor/dist/toastui-editor.css';
+// CSS
 import WriterStyles from './Writer.module.css';
 
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+import 'tui-color-picker/dist/tui-color-picker.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+
+// import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
 // import Prism from 'prismjs';
 // // 여기 css를 수정해서 코드 하이라이팅 커스텀 가능
 // import 'prismjs/themes/prism.css';
-// import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
-// import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
-
-import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
-import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
 
 export default function Writer() {
   const Editor = dynamic(
@@ -22,10 +27,7 @@ export default function Writer() {
     { ssr: false },
   );
 
-  // const colorSyntax = dynamic(
-  //   () => import('@toast-ui/editor-plugin-color-syntax'),
-  //   { ssr: false },
-  // );
+  const colorSyntax = dynamic(() => import({ colorSyntax }), { ssr: false });
 
   return (
     <>
@@ -54,6 +56,7 @@ export default function Writer() {
             initialEditType="wysiwyg"
             useCommandShortcut={false}
             plugins={[colorSyntax]}
+            // language="ko-KR"
           />
         </form>
       </section>

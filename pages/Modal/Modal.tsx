a6fc3880914778db7725596383d6db5f573551e7
younghyunHan <atuniverse@naver.com>
2022-11-26 18:48:39 +0900

    Add: Writer 컴포넌트 post title, thumbnail 링크, 파일 추가 코드 구현

--- pages/Modal/Modal.tsx
+++ pages/Modal/Modal.tsx
@@ -3,163 +3,165 @@ import ModuleStyles from './Modal.module.css';
 import axios from 'axios';
 
 export default function Modal({ setModalOpen, handleUserData }) {
-  // const access_token = localStorage.getItem('token');
-  // const modalRef = useRef<HTMLFormElement>(null);
-  // const inputRef = useRef<HTMLInputElement | null>(null);
+  const access_token = localStorage.getItem('token');
+  const modalRef = useRef<HTMLFormElement>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
 
-  // // 이름, 비밀번호
-  // const [name, setName] = useState<string>('');
-  // const [password, setPassword] = useState<string>('');
+  // 이름, 비밀번호
+  const [name, setName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
-  // //오류 메시지 상태저장
-  // const [nameMessage, setNameMessage] = useState<string>('');
-  // const [passwordMessage, setPasswordMessage] = useState<string>('');
+  //오류 메시지 상태저장
+  const [nameMessage, setNameMessage] = useState<string>('');
+  const [passwordMessage, setPasswordMessage] = useState<string>('');
 
-  // // 유효성 검사
-  // const [isName, setIsName] = useState<boolean>(false);
-  // const [isPassword, setIsPassword] = useState<boolean>(false);
+  // 유효성 검사
+  const [isName, setIsName] = useState<boolean>(false);
+  const [isPassword, setIsPassword] = useState<boolean>(false);
 
-  // useEffect(() => {
-  //   // 이벤트 핸들러 함수
-  //   const clickModalOutside = (event: any) => {
-  //     // mousedown 이벤트가 발생한 영역이 모달창이 아닐 때, 모달창 제거 처리
-  //     if (modalRef.current && !modalRef.current.contains(event.target)) {
-  //       setModalOpen(false);
-  //     }
-  //   };
+  useEffect(() => {
+    // 이벤트 핸들러 함수
+    const clickModalOutside = (event: any) => {
+      // mousedown 이벤트가 발생한 영역이 모달창이 아닐 때, 모달창 제거 처리
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        setModalOpen(false);
+      }
+    };
 
-  //   // 이벤트 핸들러 등록
-  //   document.addEventListener('mousedown', clickModalOutside);
-  //   // 지정한 이벤트가 대상에 전달될 때마다 호출할 함수를 설정
-  //   // 스스로 종료되지 않기 때문에 removeEventListener를 명시해야 종료할 수 있다
-  //   // document.addEventListener : 전체 document 페이지에 대한 이벤트이므로,
-  //   // 특정 컴포넌트 안에서만 발생된다는 착각은 금물!!
+    // 이벤트 핸들러 등록
+    document.addEventListener('mousedown', clickModalOutside);
+    // 지정한 이벤트가 대상에 전달될 때마다 호출할 함수를 설정
+    // 스스로 종료되지 않기 때문에 removeEventListener를 명시해야 종료할 수 있다
+    // document.addEventListener : 전체 document 페이지에 대한 이벤트이므로,
+    // 특정 컴포넌트 안에서만 발생된다는 착각은 금물!!
 
-  //   return () => {
-  //     // 이벤트 핸들러 해제
-  //     document.removeEventListener('mousedown', clickModalOutside);
-  //   };
-  // });
+    return () => {
+      // 이벤트 핸들러 해제
+      document.removeEventListener('mousedown', clickModalOutside);
+    };
+  });
 
-  // // 이름
-  // const onChangeName = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-  //   setName(e.target.value);
-  //   if (e.target.value.length < 2 || e.target.value.length > 5) {
-  //     setNameMessage('2글자 이상 5글자 미만으로 입력해주세요.');
-  //     setIsName(false);
-  //   } else {
-  //     setNameMessage('올바른 이름 형식입니다 :)');
-  //     setIsName(true);
-  //   }
-  // }, []);
+  // 이름
+  const onChangeName = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+    if (e.target.value.length < 2 || e.target.value.length > 5) {
+      setNameMessage('2글자 이상 5글자 미만으로 입력해주세요.');
+      setIsName(false);
+    } else {
+      setNameMessage('올바른 이름 형식입니다 :)');
+      setIsName(true);
+    }
+  }, []);
 
-  // // 비밀번호
-  // const onChangePassword = useCallback(
-  //   (e: React.ChangeEvent<HTMLInputElement>) => {
-  //     const passwordRegex =
-  //       /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
-  //     const passwordCurrent = e.target.value;
-  //     setPassword(passwordCurrent);
+  // 비밀번호
+  const onChangePassword = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const passwordRegex =
+        /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+      const passwordCurrent = e.target.value;
+      setPassword(passwordCurrent);
 
-  //     if (!passwordRegex.test(passwordCurrent)) {
-  //       setPasswordMessage(
-  //         '숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!',
-  //       );
-  //       setIsPassword(false);
-  //     } else {
-  //       setPasswordMessage('안전한 비밀번호에요 : )');
-  //       setIsPassword(true);
-  //     }
-  //   },
-  //   [],
-  // );
+      if (!passwordRegex.test(passwordCurrent)) {
+        setPasswordMessage(
+          '숫자+영문자+특수문자 조합으로 8자리 이상 입력해주세요!',
+        );
+        setIsPassword(false);
+      } else {
+        setPasswordMessage('안전한 비밀번호에요 : )');
+        setIsPassword(true);
+      }
+    },
+    [],
+  );
 
-  // const onUploadUserInfoButtonClick = useCallback(
-  //   (event: any) => {
-  //     event.preventDefault();
-  //     if (!inputRef.current) {
-  //       return;
-  //     }
+  const onUploadUserInfoButtonClick = useCallback(
+    (event: any) => {
+      event.preventDefault();
+      if (!inputRef.current) {
+        return;
+      }
 
-  //     // console.log(inputRef.current?.files?.[0].name);
-  //     const files = inputRef.current.files as FileList;
+      // console.log(inputRef.current?.files?.[0].name);
+      const files = inputRef.current.files as FileList;
 
-  //     const formData = new FormData();
-  //     // formData.append('image', inputRef.current?.files?.[0].name);
-  //     formData.append('user_img', files[0]);
-  //     formData.append('user_name', name);
-  //     formData.append('user_pw', password);
+      const formData = new FormData();
+      // formData.append('image', inputRef.current?.files?.[0].name);
+      formData.append('user_img', files[0]);
+      formData.append('user_name', name);
+      formData.append('user_pw', password);
 
-  //     axios
-  //       .post('http://localhost:3000/userInfoUpdate', formData, {
-  //         headers: {
-  //           Authorization: `${access_token}`,
-  //           'Content-Type': 'multipart/form-data',
-  //         },
-  //       })
-  //       .then(function (response) {
-  //         if (response.data.message === 'SUCCESS') {
-  //           alert('변경되었습니다.');
-  //           setModalOpen(false);
-  //           handleUserData(
-  //             response.data.user_name,
-  //             response.data.user_img.filename,
-  //           );
-  //         }
-  //       });
-  //   },
-  //   [name, password],
-  // );
+      axios
+        .post('http://localhost:3000/userInfoUpdate', formData, {
+          headers: {
+            Authorization: `${access_token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then(function (response) {
+          if (response.data.message === 'SUCCESS') {
+            alert('변경되었습니다.');
+            setModalOpen(false);
+            handleUserData(
+              response.data.user_name,
+              response.data.user_img.filename,
+            );
+          }
+        });
+    },
+    [name, password],
+  );
 
   return (
-    // <form ref={modalRef} id={ModuleStyles.modal}>
-    //   <div id={ModuleStyles.modalWrap}>
-    //     <input
-    //       id={ModuleStyles.userName}
-    //       type="text"
-    //       name="user_name"
-    //       placeholder="닉네임"
-    //       onChange={onChangeName}
-    //     />
-    //     {name.length > 0 && (
-    //       <span
-    //         className={`${
-    //           isName ? `${ModuleStyles.success}` : `${ModuleStyles.error}`
-    //         }`}
-    //       >
-    //         {nameMessage}
-    //       </span>
-    //     )}
-    //     <input
-    //       id={ModuleStyles.userPassword}
-    //       type="password"
-    //       name="user_pw"
-    //       placeholder="비밀번호 (숫자 + 영문자 + 특수문자 조합으로 8자리 이상)"
-    //       onChange={onChangePassword}
-    //     />
-    //     {password.length > 0 && (
-    //       <span
-    //         className={`${
-    //           isPassword ? `${ModuleStyles.success}` : `${ModuleStyles.error}`
-    //         }`}
-    //       >
-    //         {passwordMessage}
-    //       </span>
-    //     )}
-    //     <input
-    //       id={ModuleStyles.userImg}
-    //       type="file"
-    //       accept="image/*"
-    //       ref={inputRef}
-    //     />
-    //   </div>
-    //   <button
-    //     id={ModuleStyles.editSubmitBtn}
-    //     onClick={onUploadUserInfoButtonClick}
-    //   >
-    //     개인 정보 수정{' '}
-    //   </button>
-    // </form>
-    <div></div>
+    <>
+      <form ref={modalRef} id={ModuleStyles.modal}>
+        <div id={ModuleStyles.modalWrap}>
+          <input
+            id={ModuleStyles.userName}
+            type="text"
+            name="user_name"
+            placeholder="닉네임"
+            onChange={onChangeName}
+          />
+          {name.length > 0 && (
+            <span
+              className={`${
+                isName ? `${ModuleStyles.success}` : `${ModuleStyles.error}`
+              }`}
+            >
+              {nameMessage}
+            </span>
+          )}
+          <input
+            id={ModuleStyles.userPassword}
+            type="password"
+            name="user_pw"
+            placeholder="비밀번호 (숫자 + 영문자 + 특수문자 조합으로 8자리 이상)"
+            onChange={onChangePassword}
+          />
+          {password.length > 0 && (
+            <span
+              className={`${
+                isPassword ? `${ModuleStyles.success}` : `${ModuleStyles.error}`
+              }`}
+            >
+              {passwordMessage}
+            </span>
+          )}
+          <input
+            id={ModuleStyles.userImg}
+            type="file"
+            accept="image/*"
+            ref={inputRef}
+          />
+        </div>
+        <button
+          id={ModuleStyles.editSubmitBtn}
+          onClick={onUploadUserInfoButtonClick}
+        >
+          개인 정보 수정{' '}
+        </button>
+      </form>
+      <div></div>
+    </>
   );
 }
--- pages/Writer/Writer.tsx
+++ pages/Writer/Writer.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from 'react';
 import axios from 'axios';
 import dynamic from 'next/dynamic';
 
@@ -17,6 +16,10 @@ import 'tui-color-picker/dist/tui-color-picker.css';
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
 
 export default function Writer() {
+  const access_token = localStorage.getItem('token');
+  const [postTitle, setPostTitle] = useState<string>('');
+  const [postThumbnailLink, setPostThumbnailLink] = useState<string>('');
+
   const Editor = dynamic(
     () => import('@toast-ui/react-editor').then(m => m.Editor),
     { ssr: false },
@@ -26,6 +29,55 @@ export default function Writer() {
   //   () => import('@toast-ui/editor-plugin-color-syntax'),
   //   { ssr: false },
   // );
+
+  const postThumbnailInputRef = useRef<HTMLInputElement | null>(null);
+  // console.log(postThumbnailInputRef.current.files[0]);
+
+  const changePostTitle = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPostTitle(e.target.value);
+    },
+    [],
+  );
+
+  const changePostThumbnailLink = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPostThumbnailLink(e.target.value);
+    },
+    [],
+  );
+
+  const postUploadButtonClick = useCallback(
+    (event: any) => {
+      event.preventDefault();
+      if (!postThumbnailInputRef.current) {
+        return;
+      }
+
+      const files = postThumbnailInputRef.current.files as FileList;
+
+      const formData = new FormData();
+      console.log(postTitle);
+      console.log(postThumbnailLink);
+      formData.append('postThumnailImg', files[0]);
+      formData.append('postTitle', postTitle);
+      formData.append('postThumbnailLink', postThumbnailLink);
+
+      axios
+        .post('http://localhost:3000/userInfoUpdate', formData, {
+          headers: {
+            Authorization: `${access_token}`,
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+        .then(function (response) {
+          if (response.data.message === 'SUCCESS') {
+            alert('게시글 저장되었습니다.');
+          }
+        });
+    },
+    [postTitle, postThumbnailLink],
+  );
 
   return (
     <>
@@ -37,16 +89,27 @@ export default function Writer() {
           <h2 id={WriterStyles.postTitle}>Title</h2>
           <input
             id={WriterStyles.postTitleInput}
+            type="text"
+            name="postTitle"
             placeholder="제목을 입력해주세요."
+            onChange={changePostTitle}
           />
           <h2 id={WriterStyles.postThumbnail}>Thumbnail</h2>
           <div id={WriterStyles.postThumnailWrap}>
             <input
               id={WriterStyles.postThumnailInput}
+              type="text"
+              name="postThumbnailLink"
               placeholder="링크를 입력해주세요."
+              onChange={changePostThumbnailLink}
             />
             <span>or</span>
-            <input id={WriterStyles.postThumnailBtn} type="file" />
+            <input
+              id={WriterStyles.postThumnailBtn}
+              type="file"
+              accept="image/*"
+              ref={postThumbnailInputRef}
+            />
           </div>
           <Editor
             initialValue="hello react editor world!"
@@ -57,6 +120,12 @@ export default function Writer() {
             // plugins={[colorSyntax]}
             // language="ko-KR"
           />
+          <button
+            id={WriterStyles.postUploadBtn}
+            onClick={postUploadButtonClick}
+          >
+            개인 정보 수정
+          </button>
         </form>
       </section>
     </>

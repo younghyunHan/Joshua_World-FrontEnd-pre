73f84bb427c989c1ab3a14b5639146253c48cd26
younghyunHan <atuniverse@naver.com>
2022-11-28 19:20:11 +0900

    Fix: Pages 폴더내 폴더, 컴포넌트 명 camelCase로 변경

--- Components/Editor/Editor.tsx
+++ Components/Editor/Editor.tsx
@@ -12,10 +12,8 @@ import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-sy
 // code syntax highlight
 import 'prismjs/themes/prism.css';
 import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.css';
-
 import Prism from 'prismjs'; // prism 테마 추가
-
-import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
+import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin-code-syntax-highlight.js';
 
 // table-merged-cell
 import tableMergedCell from '@toast-ui/editor-plugin-table-merged-cell';
